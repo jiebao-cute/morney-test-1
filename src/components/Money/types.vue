5e1f0dcbf6a898c8ad496339a50141ea7be92af0
jiebao <908822571@qq.com>
2020-12-17 00:26:23 +0800

    numberPad 组件功能实现 除去OK功能

--- src/components/Money/types.vue
+++ src/components/Money/types.vue
@@ -14,21 +14,15 @@
    import  Vue from 'vue';
    import {Component} from 'vue-property-decorator';
 
-   @Component({
-     props:{
-     propMessage:String
-     }
-   })
+   @Component
    export default class Types extends  Vue {
      type = '-';//‘-’表示支出，‘+’表示收入
-     helloMsg = 'Hello, '+ this.propMessage;
      selectType(type: string){
        if(type !== '-' && type !== '+'){//type只能是‘-’‘+’
          throw new Error('type is unknown')
        }
        this.type=type;
      }
-
    }
 
 //js语法
