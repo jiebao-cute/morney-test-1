e501cc825e200af82cf09979798a4aa1936d02f8
jiebao <908822571@qq.com>
2020-12-25 00:32:59 +0800

    可以编辑标签并且自动保存

--- src/models/tagsListModel.ts
+++ src/models/tagsListModel.ts
@@ -8,6 +8,7 @@ type TagsListModel ={
   fetch: () => Tag[];
   create: (name: string) => 'success'|'duplicated';
   save: () => void;
+  update: (id: string ,name: string) => 'success'|'not found'|'duplicated';
 }
 const tagsListModel: TagsListModel= {
   data:[],
@@ -17,6 +18,22 @@ const tagsListModel: TagsListModel= {
     this.data.push({id:name,name:name});
     this.save();
     return 'success';
+  },
+  update(id,name) {
+    const idList = this.data.map(item => item.id);
+    if (idList.indexOf(id) >= 0){
+      const names = this.data.map(item => item.name);
+      if (names.indexOf(name) >= 0){
+        return 'duplicated';
+      }else {
+        const tag = this.data.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return 'success';
+      }
+    }else{
+      return 'not found';
+    }
   },
   fetch(){
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,10 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper" >
-    <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+    <FormItem :value="tag.name"
+              @update:value="update"
+              field-name="标签名"
+              placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
       <Button>删除标签</Button>
@@ -36,6 +39,12 @@ export default class EditLabel extends Vue{
       this.$router.replace('/404');
     }
   }
+    update(name: string){
+      if(this.tag){
+        console.log(this.tag);
+        tagsListModel.update(this.tag.id,name);
+      }
+    }
 
 }
 </script>
