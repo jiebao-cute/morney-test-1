3ab037d9bd972eece61b66fb3e5dced4b8b0cc25
jiebao <908822571@qq.com>
2020-12-28 16:51:54 +0800

    封装 record 的操作

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -5,10 +5,11 @@ const recordListModel = {
 
   data:[] as RecordItem[],
 
-  create(record: RecordItem[]){
+  create(record: RecordItem){
     const record2: RecordItem = clone(record);
     record2.createdAt = new Date();
     this.data.push(record2);
+    this.save();
   },
 
   fetch() {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,17 +16,14 @@ import NumberPad from '@/components/Money/numberPad.vue';
 import Types from '@/components/Money/types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
+import {Component} from 'vue-property-decorator';
 
-
-const recordList = recordListModel.fetch();
 @Component({
   components: {Tags,  FormItem, Types, NumberPad}
 })
 export default class Money extends Vue{
       tags = window.tagList;
-      recordList: RecordItem[] = recordList;
+      recordList = window.recordList();
       record: RecordItem ={
         tags:[] , notes:'' , type:'-' , amount:0
       }
@@ -37,11 +34,7 @@ export default class Money extends Vue{
          this.record.notes = value;
        }
        saveRecord(){
-          recordListModel.create(this.record)
-       }
-      @Watch('recordList')
-         onRecordListChange(){
-         recordListModel.save();
+        window.createRecord(this.record);
       }
     }
 </script>
