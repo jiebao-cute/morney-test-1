f7371fbe638d85d3820c4813570f0f2e4758694d
jiebao <908822571@qq.com>
2020-12-28 17:56:30 +0800

    将所有数据操作封装在 store 里

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -17,12 +17,3 @@ type TagsListModel ={
   update: (id: string ,name: string) => 'success'|'not found'|'duplicated';
   remove: (id: string) => boolean;
 }
-interface Window {
-  tagList: Tag[];
-  createTag: (name: string) => void;
-  findTag: (id: string) => Tag | undefined;
-  removeTag: (id: string) => boolean;
-  updateTag: (id: string ,name: string) => 'success'|'not found'|'duplicated';
-  recordList: RecordItem[];
-  createRecord: (record: RecordItem) => void;
-}
