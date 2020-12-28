c74247a3f58a3a7fbbc324a61fe604f62ecc491a
jiebao <908822571@qq.com>
2020-12-28 18:06:43 +0800

    模块化的 store

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,32 +1,11 @@
-import recordListModel from '@/models/recordListModel';
-import tagsListModel from '@/models/tagsListModel';
+
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  // record store
-  recordList : recordListModel.fetch(),
+  ...recordStore,
+  ...tagStore,
 
-  createRecord : (record: RecordItem)=> recordListModel.create(record),
-
-  // tag store
-  tagList : tagsListModel.fetch(),
-
-  findTag(id: string){
-    return this.tagList.filter(t=>t.id ===id)[0];
-  },
-  createTag :(name: string)=>{
-    const message = tagsListModel.create(name);
-    if (message === 'duplicated'){
-      window.alert('标签名重复了');
-    }else if (message === 'success'){
-      window.alert('添加成功')
-    }
-  },
-  removeTag: (id: string) =>{
-    return tagsListModel.remove(id);
-  },
-  updateTag : (id: string ,name: string) => {
-    return tagsListModel.update(id,name);
-  }
 };
 
 export  default store;
