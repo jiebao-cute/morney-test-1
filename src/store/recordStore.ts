17f9dc152e5870d24d39d29f261a5468cf6e7f74
jiebao <908822571@qq.com>
2020-12-29 17:51:22 +0800

    模块化的 store，删除了 models

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,9 +1,23 @@
-import recordListModel from '@/models/recordListModel';
+import clone from '@/lib/clone';
 
-export default {
+const localStorageKeyName = 'recordList';
+
+const recordStore ={
   // record store
-  recordList : recordListModel.fetch(),
-
-  createRecord : (record: RecordItem)=> recordListModel.create(record),
-
-}
+  recordList : [] as RecordItem[],
+  fetchRecord(){
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
+    },
+  saveRecords(){
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
+  },
+  createRecord(record: RecordItem) {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.recordList && this.recordList.push(record2);
+    this.saveRecords();
+  }
+};
+  recordStore.fetchRecord();
+export default recordStore;
