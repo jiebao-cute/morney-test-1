f7e44ee943dc42ed19708d60af0ee3c7053881ab
jiebao <908822571@qq.com>
2021-01-12 23:24:20 +0800

    Statistics.vue页面CSS的完成

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -47,7 +47,7 @@ export default class Money extends Vue{
       }
     }
 </script>
-<style lang="scss">
+<style lang="scss" >
     .xxx-content{
       display: flex;
       flex-direction: column-reverse;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,18 +3,18 @@
   <layout>
      <Tabs class-prefix = "type" :data-source="typeList" :value.sync="type"/>
      <Tabs class-prefix = "interval" :data-source="intervalList" :value.sync="interval" height='48px'/>
-    <div>
       <ol>
-        <li v-for="(group,index) in result" :key="index">
-         <H3>{{group.title}}</H3>
+        <li v-for="(group,index) in result" :key="index" >
+         <H3 class="title">{{group.title}}</H3>
           <ol>
-            <li v-for="item in group.items" :key="item.id">
-              {{item.amount}} {{item.createdAt}}
+            <li v-for="item in group.items" :key="item.id" class="record">
+              <span>{{tagString(item.tags)}}</span>
+              <span class="note">{{item.notes}}</span>
+              <span>{{item.amount}}</span>
             </li>
           </ol>
         </li>
       </ol>
-    </div>
   </layout>
 </div>
 </template>
@@ -31,6 +31,20 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs}
 })
 export default class Statistics extends Vue{
+  tagString(tags: Tag[]) {
+    if(tags.length <= 0){
+      return '空';
+    }else{
+      let a = '';
+      for(let i =0;i<tags.length ;i++){
+        if(tags[i].name){
+          a = a + tags[i].name + ','
+        }
+      }
+      return a.substr(0,a.length-1)
+    }
+
+  }
 
   get recordList() {
     return (this.$store.state as RootState).recordList;//这里获取数据的时候会出现类型错误，所以需要声明
@@ -57,7 +71,29 @@ export default class Statistics extends Vue{
   typeList = recordTypeList;
 }
 </script>
+<style lang="scss" scoped>
+%item{
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+.title{
+  @extend %item;
 
+}
+.record{
+  background: white;
+  @extend %item;
+
+}
+.note{
+margin-right: auto;
+  margin-left: 16px;
+  color: #999999;
+}
+</style>
 <style lang="scss" scoped>
  ::v-deep {
    .type-tabs-item{
