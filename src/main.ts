7f3566104941c18611aa0e1e267d7db842c6cd16
jiebao <908822571@qq.com>
2021-01-14 20:40:45 +0800

    二维码体验感不好，删除二维码，将pc端宽度改小

--- src/main.ts
+++ src/main.ts
@@ -24,14 +24,3 @@ window.onload = function (){
   },0);
 };//浏览器如果无法看到最下方的标签，可让他在0秒置底
 
-if(document.documentElement.clientWidth > 500){
-  window.alert('请使用手机打开页面，以保证浏览效果');
-  const img = document.createElement('img');
-  img.src = './qrcode.png';
-  img.style.position = 'fixed';
-  img.style.left = '50%';
-  img.style.top = "50%";
-  img.style.transform = 'translate(-50%,-50%)';
-  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
-  document.body.appendChild(img);
-}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
       ? '/morney-1-website/'
