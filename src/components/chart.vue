f09edf976209fbd566c85571bdf204cd39f8b88c
jiebao <908822571@qq.com>
2021-01-19 17:06:40 +0800

    自动更新 options

--- src/components/chart.vue
+++ src/components/chart.vue
@@ -4,21 +4,26 @@
 <script lang="ts">
 
 
-import {Component,Prop,Vue} from 'vue-property-decorator';
-import echarts,{EChartsOption} from  'echarts';
+import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
+import echarts,{EChartsOption,ECharts} from  'echarts';
+
 
 @Component
 export default class Chart extends Vue{
   @Prop() options?: EChartsOption;
+  chart?: ECharts;
   mounted(){
     if (this.options === undefined){
       return console.error('options为空')
     }
     const echarts = require('echarts')
-    const chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
-    chart.setOption(this.options)
+    this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
+    this.chart.setOption(this.options)
   }
-
+  @Watch('options')
+  onOptionsChange(newValue: EChartsOption){
+    this.chart!.setOption(newValue);
+  }
  }
 </script>
 <style scoped lang="scss">
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -77,11 +77,11 @@ export default class Statistics extends Vue {
    const array = [];
    for (let i = 0 ; i<=29 ; i++){
      const dateString = day(today).subtract(i,'day').format('YYYY-MM-DD');
-     const found = _.find(this.recordList,{
-       createdAt: dateString
+     const found = _.find(this.groupedList,{
+       title: dateString
      });
      array.push({
-       date:dateString,value: found ? found.amount : 0
+       date:dateString,value: found ? found.total : 0
      });
    }
    array.sort((a,b)=>{
